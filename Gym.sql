9519ef2d6815bfa3506789f64f51051c670144f1
valen <valensenefacultad@gmail.com>
2026-07-03 11:11:00 -0300

    actualizo archivo gym.sql

--- Gym.sql
+++ Gym.sql
@@ -14,19 +14,30 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- La exportación de datos fue deseleccionada.
 
--- La exportación de datos fue deseleccionada.
+-- Volcando estructura de base de datos para gestiongimnasio
+CREATE DATABASE IF NOT EXISTS `gestiongimnasio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `gestiongimnasio`;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla gestiongimnasio.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `DNI` int(11) NOT NULL,
+  `sexo` char(50) DEFAULT NULL,
+  `observaciones` varchar(50) DEFAULT NULL,
+  `perfil` int(11) NOT NULL,
+  `nota1` varchar(50) DEFAULT NULL,
+  `nota2` varchar(50) DEFAULT NULL,
+  `usuLogin` varchar(50) NOT NULL,
+  `usuPsw` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `perfil` (`perfil`),
+  CONSTRAINT `perfil` FOREIGN KEY (`perfil`) REFERENCES `perfiles` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
-
--- La exportación de datos fue deseleccionada.
-
--- La exportación de datos fue deseleccionada.
-
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla gestiongimnasio.usuarios: ~0 rows (aproximadamente)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
